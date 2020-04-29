f67f8c4a0ef19334b7b82c28a69c65f573c1d6e4
Haijing <liuh494@newschool.edu>
2020-04-29 23:13:57 +0800

    sorryforhurtingppl
    friends where are you

---
+++ src/practise2.js
@@ -0,0 +1,10 @@
+// Modern JavaScript Tutorial #3 Control Flow
+// Youtube reference: https://www.youtube.com/watch?v=JloLGV9DmtQ
+// All JS codes are followed with Youtube Bogger: TheNetNinja 's Tutorial
+
+
+// Control Flow Briefs
+// Use Loops for cycling through data
+// ["bless""happiness""wedding"] looping toUpperCase()
+// Use Conditional Statements for checking conditions
+// if(some condition is true){ do something ...}
