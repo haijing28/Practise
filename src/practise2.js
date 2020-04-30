74b0bc96ee0cc6fe9cf8f91f406be6eac13329d7
haijing28 <liuh494@newschool.edu>
2020-04-30 14:54:57 +0800

    odified

--- src/practise2.js
+++ src/practise2.js
@@ -147,3 +147,51 @@ console.log("jumpping 0",scores[a]);
 
 // will do switch on the last of April .
 // Good night 
+
+// switch statement
+// checking multiple possible values of a single variable is to use switch statement.
+
+
+// const grade = "A";
+
+  let grade = "50";
+
+
+  grade = Number(grade);
+
+  switch(grade){ // avoid of rewritting if else statement over & over again. 
+     
+      case 50:
+       console.log("You got an A!");  //using case 
+       break;
+      
+      case "B":
+       console.log("You got an B!");
+      break;
+      
+      case "C":
+       console.log("You got an C!");
+      break;
+      
+      case "D":
+       console.log("You got an D!");
+       break;
+      
+      case "E":
+       console.log("You got an E!");
+       break;
+      
+      default:
+       console.log("not a valid grade");
+
+  }
+
+
+
+
+
+
+// Using if statement
+
+
+
