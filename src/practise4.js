0e644de09319b3bfa09c45f8915fcb7db364b7b3
Haijing <liuh494@newschool.edu>
2020-05-02 00:07:29 +0800

    Update practise4.js
    objects

--- src/practise4.js
+++ src/practise4.js
@@ -1,3 +1,20 @@
 // Modern JavaScript Tutorial #4 - Objects
 // Youtube reference: https://www.youtube.com/watch?v=X0ipw1k7ygU
 // All JS codes are followed with Youtube Bogger: TheNetNinja 's Tutorial
+
+
+//  Intro to OBJECTS
+//  what is an Object ?
+//  comparing it with objects in the real life.
+//  real object, eg, a phone, has properties (eg: -color, -size, -model, -etc),
+//                           has function (or 'd say has things todo, eg: -ring, -take a photo, -make a call, -etc)
+
+//  Objects in javascript have properties & things they can do(methods)
+
+//  JS Object
+//  eg1. a User Object
+//    - Properties: emailaddress, username, gender
+//    - Methods: login, logout
+//  eg2. a Blog Object
+//    - Properties: title, content, author
+//    - Methods: publish, unpublish, delete
