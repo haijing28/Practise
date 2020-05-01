0ad79c0b8156ccf8ff90c054cbcf478c4e4c3149
Haijing <liuh494@newschool.edu>
2020-05-01 23:40:41 +0800

    add practise4.js

---
+++ src/practise4.js
@@ -0,0 +1,3 @@
+// Modern JavaScript Tutorial #4 - Objects
+// Youtube reference: https://www.youtube.com/watch?v=X0ipw1k7ygU
+// All JS codes are followed with Youtube Bogger: TheNetNinja 's Tutorial
