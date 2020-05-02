be0c457133338a0a48e2c224e507f3821a87eb3e
Haijing <liuh494@newschool.edu>
2020-05-03 01:00:17 +0800

    DOM
    notes

--- src/practise5.js
+++ src/practise5.js
@@ -1,3 +1,25 @@
 // Modern JavaScript Tutorial #6 - The Document Object Model
 // Youtube reference: https://www.youtube.com/watch?v=wKBu_dEaF9E
 // All JS codes are followed with Youtube Bogger: TheNetNinja 's Tutorial
+
+
+// Interacting with browser
+// add content to the browser
+// change css style
+// react to user events (eg.clicking)
+// cool effects like popups
+// improving internet user experience is the initial purpose
+// that javascript has been originally created.
+
+// Document Object Model (DOM)
+// Add, Change & Delete Content
+// Make a cool pop-up effect in the web page
+
+
+
+// Intro DOM
+// - heart of webpage manipulation
+// - essentially create by the browser when an html document loads inside it
+// - interact with HTML document from our javascript code to do things
+// - browser models html document, so it's called "document" object model
+// - DOM contains many properties & methods we can use to interact with the html5
